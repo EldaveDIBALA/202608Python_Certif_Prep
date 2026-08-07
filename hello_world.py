147cd15652a850041e2869b61bbc4f38a976f3b7
Eldave DIBALA <eldavedibala@gmail.com>
2026-08-08 00:42:46 +0100

    feat: uniformisation

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,42 @@
+print("Hello, World!\nThis is a new departure with coding.")
+print()
+print("I wish" , "myself" , "a good luck with" , "my Python journey.")
+
+print()
+print("Python vient de", "Monthy Python.", end=" ") # Keyword
+print("Une idée de", "Guido Van ROSSUM.")
+
+print()
+print("Ici, j'essaye de faire en sorte que :", end="\n") # Keyword
+print("* deux argument sur deux lignes de codes, se retrouvent quan même sur deux lignes.")
+
 print("Pour citer, on utilise :\n", "- les deux points ;", end=" ")
 print(" - le point virgule.")
 
 print()
 print("Pour citer, on utilise :\n", "- les deux points ;", end="\n")
 print(" - le point virgule.")
+
+print()
+print("11111111\n")
+print("11", "111", "111\n", sep="_") # Keyword
+
+print(11111111, "\n")
+print(11_111_111)
+
+print()
+# Notions de Octal et Hexadécial :
+print(0o123, "\n") # Octal
+print(0x123, "\n") #Hexadécimal
+
+# Notion de Floats
+print(-2.4)
+print("\n", .5)
+print("\n", -4.)
+
+# Notion d'exposant
+print("\n", 3E8)
+print("\n", 6.2607E-34)
 
 print("\n", 0.0000000000000000000001)
 

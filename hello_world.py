22324a783c9034bf72eee002ca1904d55734a791
Eldave DIBALA <eldavedibala@gmail.com>
2026-08-08 15:29:17 +0100

    feat: mise à jour du 20260807

--- hello_world.py
+++ hello_world.py
@@ -43,3 +43,8 @@ print("\n", 0.0000000000000000000001)
 # On révise les strings
 print("Jésus a dit : \"Je suis le chemin et la vérité. Nul ne peut aller au Père sanns passer par moi.\"")
 print("\n", "Notre salut est en Jésus-Christ notre Seigneur et Sauveur.", "Amen! - 'Eldave'", sep=" ")
+
+# Boolean values
+print()
+print(True > False)
+print(True < False)

559d2edeef66b3eae82799643f644e24f6ecbf1a
Eldave DIBALA <62725652+EldaveDIBALA@users.noreply.github.com>
2026-08-08 22:49:11 +0100

    feat: Operators and their priorities.

---
+++ operators_priorities.py
@@ -0,0 +1,11 @@
+# La hierarchie des priorités ordonnance l'action de certains opérateurs avant d'autres.
+# Par exemple, la multiplication vient avant l'addition.
+
+# Avec Python, le calcul se fait de la gauche vers la droite.
+# Par exemple :
+print(9 % 6 % 2)
+print()
+
+# Operators and their bindings: exponentiation
+print(2 ** 2 ** 3)
+print()

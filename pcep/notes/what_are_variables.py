f3ef0b0e45c2a5e0f1dce0bdbd7d4a71bb22bf7a
Eldave DIBALA <eldavedibala@gmail.com>
2026-08-13 00:09:03 +0100

    feat: Update sur les variables.

--- pcep/notes/what_are_variables.py
+++ pcep/notes/what_are_variables.py
@@ -5,3 +5,20 @@ print("/Users/eldavedibala/Documents/Engineer/SDET/202608Python_Certif_Prep/pcep
 print()
 
 print(__file__)
+print()
+
+var = 2026
+print(var - 1990)
+print()
+
+var = var + 1
+print(var - 1990)
+print()
+
+# Le théorème de Pythagore appliqué au triangel rectangle ABC
+
+cote_ab = 5
+cote_bc = 12
+hypothenus = (cote_ab ** 2 + cote_bc ** 2) ** .5
+print("Les valeurs des côtés AB et BC étant égale respectivement à", cote_ab, "et", cote_bc, ", alors l'hypothénus est égale à :", hypothenus)
+print()

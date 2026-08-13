c214a8665afb0d253b58af2828f2c32b0c10f225
Eldave DIBALA <eldavedibala@gmail.com>
2026-08-13 01:52:23 +0100

    feat: Shortcut operators.

--- pcep/notes/what_are_variables.py
+++ pcep/notes/what_are_variables.py
@@ -15,10 +15,40 @@ var = var + 1
 print(var - 1990)
 print()
 
-# Le théorème de Pythagore appliqué au triangel rectangle ABC
+# Le théorème de Pythagore appliqué au triangle rectangle ABC :
 
 cote_ab = 5
 cote_bc = 12
 hypothenus = (cote_ab ** 2 + cote_bc ** 2) ** .5
 print("Les valeurs des côtés AB et BC étant égale respectivement à", cote_ab, "et", cote_bc, ", alors l'hypothénus est égale à :", hypothenus)
+print()
+
+
+# Shortcut operators
+
+x = 18
+x = x * 2 # x = 36
+
+y = 18
+y *= 2 # y = 36
+
+ab = 1990
+ab = ab + 36 # ab = 2026
+
+bc = 1990
+bc += 36 # bc = 2026
+
+print("En utilisant les raccourcis, x = y et ab = bc")
+print()
+print("En voici la preuve :\n", "\nx = ", x, ";\ny = ", y, ";\nab =", ab, ";\net bc =", bc, end="\n")
+print()
+
+# LAB
+
+x = float(input("Quelle valeur veux-tu assigner à x ?\n"))
+print("\nx =", x)
+print()
+
+y = (3 * (x ** 3)) - (2 * (x ** 2)) + (3 * x) - 1
+print("y =", y)
 print()

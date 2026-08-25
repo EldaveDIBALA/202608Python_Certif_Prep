6b70fe4ed417a302eac0b933d9d5163827135abd
Eldave DIBALA <eldavedibala@gmail.com>
2026-08-25 11:40:05 +0100

    feat: Terminé avec la console Python.

--- 02. pcep/notes/key_takeaways_continued.py
+++ 02. pcep/notes/key_takeaways_continued.py
@@ -3,12 +3,6 @@ import time
 
 ############################################################################################################################
 
-
-
-print(f"\n{"=" * 68}")
-
-############################################################################################################################
-
 """
 
 Exercise 4
@@ -19,16 +13,20 @@ each 0 with x, and print the modified string to the screen. Use the skeleton bel
 """
 
 for digit in "0165031806510":
-    
     if digit == "0":
-        modified_string = digit.replace("0", "x") # Line of code.
-        #print(digit) # Line of code.
+        print(digit.replace("0", "x")) # Line of code.
+        continue # Line of code.
+    print(digit) # Line of code.
+    
+print(f"\n{"=" * 68}")
+
+""" Autre solution """
+
+for digit in "0165031806510":
+    if digit == "0":
+        print("x", end="")
         continue
-
-    #continue # Line of code.
-    print(modified_string)
-
-sys.exit()
+    print(digit, end="")
     
 print(f"\n{"=" * 68}")
 
